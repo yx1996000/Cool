17442149b3cdcc740166a467062c36a08de2d085
Young <731930976@qq.com>
2017-06-08 17:49:07 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/example/asus/cool/util/HttpUtil.java
@@ -0,0 +1,14 @@
+package com.example.asus.cool.util;
+
+/**
+ * Created by asus on 2017-6-8.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
