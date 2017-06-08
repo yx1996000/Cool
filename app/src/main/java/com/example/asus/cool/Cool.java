35bfe2d74960e1d1c72885a0d118a4c971858a1c
Young <731930976@qq.com>
2017-06-08 16:20:44 +0800

    First commit

---
+++ app/src/main/java/com/example/asus/cool/Cool.java
@@ -0,0 +1,13 @@
+package com.example.asus.cool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cool extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cool);
+    }
+}
